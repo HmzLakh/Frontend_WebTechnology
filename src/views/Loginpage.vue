e8b497e52b6df54ccbe45b6710ed8e1c919d7572
Hamza Lakhloufi <hamza.hamza001@live.com>
2021-11-02 21:21:48 +0100

    Editing routermode + little css adjustment

--- src/views/Loginpage.vue
+++ src/views/Loginpage.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="loginpage">
     <LoginContainer></LoginContainer>
-    <a @click="$router.go(-1)">Go back</a>
+    <a class="goback" @click="$router.go(-1)">
+      <font-awesome-icon :icon="['fas', 'arrow-alt-circle-left']" /> Go back
+    </a>
   </div>
 </template>
 
@@ -25,6 +27,20 @@ export default {
   height: 100vh;
   background: rgb(195,55,100);
   background: linear-gradient(348deg, rgba(195,55,100,1) 0%, rgba(29,38,113,1) 100%);
+}
+
+.goback {
+  margin: 15px 0;
+  font-family: Nunito;
+  color: white;
+  transition: 0.25s;
+}
+
+.goback:hover {
+  color: #ffdde1;
+  font-size: 20px;
+  transition: 0.25s;
+  cursor: pointer;
 }
 
 </style>
