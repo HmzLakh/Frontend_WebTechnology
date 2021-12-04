2383a24f3d16198651d246906c5e1448050f21a0
Hamza Lakhloufi <hamza.hamza001@live.com>
2021-12-04 16:33:56 +0100

    Adding overlay

---
+++ src/components/overlay/EditPost.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="overlay-editpost">
+        <p>Images</p>
+        <p>Zalen</p>
+        <p></p>
+    </div>
+</template>
+<script>
+export default {
+  name: 'overlay-editpost'
+}
+</script>
+
+<style>
+#overlay-editpost {
+    height: 400px;
+    width: 100%;
+}
+</style>
