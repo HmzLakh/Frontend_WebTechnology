96622fdbe62188b3436ae4532e4e648e2a886db4
Hamza Lakhloufi <hamza.hamza001@live.com>
2021-12-18 10:52:23 +0100

    Adding profileviewer without logic

---
+++ src/components/profile_components/ProfileCardInformation.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="profilecard">
+        <div class="profilecard-title-card">
+            <p class="profilecard-title-card-t">{{ title }}</p>
+        </div>
+        <div class="profilecard-container">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'profilecard',
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    },
+}
+</script>
+
+<style>
+#profilecard {
+    height: 100px;
+    width: 100%;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
+
+.profilecard-title-card {
+    width: 100%;
+    border-bottom: 1px solid silver;
+}
+
+.profilecard-title-card-t {
+    font-family: Nunito;
+    font-size: 20px;
+    text-align: center;
+}
+
+.profilecard-container {
+    padding: 15px;
+}
+
+.profilecard-container-desc {
+    font-family: Nunito;
+}
+</style>
---
+++ src/components/profile_components/ProfileViewer.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="profileviewer">
+      <div class="profile-image">
+          <div class="profile-image-container">
+              <img src="https://cultivatedculture.com/wp-content/uploads/2019/12/Austin-Portrait-Taken-By-Professional-Photographer-683x1024.png" alt="Profile picture">
+          </div>
+      </div>
+      <div class="profile-informations-container">
+          <ProfileCard title="Type of profile">
+              <p>This user is a owner</p>
+          </ProfileCard>
+          <ProfileCard title="Profile information">
+              <p>Hamza Lakhloufi enrolled 01/01/1999</p>
+          </ProfileCard>
+          <ProfileCard title="Last posts (ONLY IF OWNER)">
+              <p>Show here the last posts with links</p>
+          </ProfileCard>
+      </div>
+  </div>
+</template>
+
+<script>
+import ProfileCard from './ProfileCardInformation.vue'
+
+export default {
+  name: 'profileviewer',
+  components: {ProfileCard}
+}
+</script>
+
+<style>
+#profileviewer {
+    width: 100%;
+    border: 1px solid silver;
+}
+
+.profile-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.profile-image-container {
+    margin: 15px 0;
+    height: 150px;
+    width: 150px;
+    border-radius: 100px;
+    overflow: hidden;
+}
+
+.profile-image-container img {
+    width: 100%;
+}
+
+.profile-informations-container {
+    padding: 25px;
+    width: 100%;
+}
+
+.profile-informations-container > * + * {
+    margin-top: 25px;
+}
+</style>
