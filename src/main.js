c5de4b9989c5ce81927809653293532fb0e7d15f
Hamza Lakhloufi <hamza.hamza001@live.com>
2021-12-18 18:26:06 +0100

    Adding vertical navbar menu

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import Application from './Application.vue' // Main application
 import store from './store'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStar, faEye,  faArrowAltCircleLeft, faSearch, faChevronLeft, faChevronRight, faThumbsUp, faThumbsDown, faBars, faHome, faSearchLocation, faUserEdit, faBuilding, faPlus, faEdit, faTrash, faCalendarAlt, faTimes, faSpinner} from '@fortawesome/free-solid-svg-icons'
+import { faStar, faEye,  faArrowAltCircleLeft, faSearch, faChevronLeft, faChevronRight, faThumbsUp, faThumbsDown, faBars, faHome, faSearchLocation, faUserEdit, faBuilding, faPlus, faEdit, faTrash, faCalendarAlt, faTimes, faSpinner, faInfoCircle} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faStar, faEye, faArrowAltCircleLeft, faSearch, faChevronLeft, faChevronRight, faThumbsUp, faThumbsDown, faBars, faHome, faSearchLocation, faUserEdit, faBuilding, faPlus, faEdit, faTrash, faCalendarAlt, faTimes, faSpinner)
+library.add(faStar, faEye, faArrowAltCircleLeft, faSearch, faChevronLeft, faChevronRight, faThumbsUp, faThumbsDown, faBars, faHome, faSearchLocation, faUserEdit, faBuilding, faPlus, faEdit, faTrash, faCalendarAlt, faTimes, faSpinner, faInfoCircle)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
